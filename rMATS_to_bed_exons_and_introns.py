2ca1531582195a242ea31873c87b6791b11ac7f6
tmogrady <christina.ogrady@gmail.com>
2016-09-01 17:43:30 +0200

    fixed str/int printing issue when specifying number of bases

--- rMATS_to_bed_exons_and_introns.py
+++ rMATS_to_bed_exons_and_introns.py
@@ -49,31 +49,31 @@ for line in open_file:
         chrStart = upper
         if name in SEQ: #if the exon is in the dictionary already (i.e., is a duplicate)
             if int(SEQ[name][1]) > upper: # if the upstream intron in the dictionary is shorter than the current one...
-                SEQ[name][1] = upper #...replace it with the current, longer one, keeping other info the same
+                SEQ[name][1] = str(upper) #...replace it with the current, longer one, keeping other info the same
             else:
                 pass
         else: # if the current exon is not in the dictionary, add it
-            SEQ[name] = [chr, chrStart, fields[6], fields[1]+"|"+name, fields[22], fields[4]]
+            SEQ[name] = [chr, str(chrStart), fields[6], fields[1]+"|"+name, fields[22], fields[4]]
     else: #if the size is not specified, or is larger than the intron (i.e., full intron is being returned)
         chrStart = fields[8]
         if name in SEQ:
             if int(SEQ[name][1]) > chrStart: # if the upstream intron in the dictionary is shorter than the current one...
-                SEQ[name][1] = chrStart #...replace it with the current, longer one, keeping other info the same
+                SEQ[name][1] = str(chrStart) #...replace it with the current, longer one, keeping other info the same
             else:
                 pass
         else: # if the current exon is not in the dictionary, add it
-            SEQ[name] = [chr, chrStart, fields[6], fields[1]+"|"+name, fields[22], fields[4]]
+            SEQ[name] = [chr, str(chrStart), fields[6], fields[1]+"|"+name, fields[22], fields[4]]
 # to get downstream coordinate:
     if size and (lower <= int(fields[9])):
         chrEnd = lower
         if int(SEQ[name][2]) < chrEnd: #don't need "if name in SEQ" because name will always be in SEQ from above
-            SEQ[name][2] = chrEnd
+            SEQ[name][2] = str(chrEnd)
         else:
             pass
     else:
         chrEnd = fields[9]
         if int(SEQ[name][2]) < chrEnd:
-            SEQ[name][2] = chrEnd
+            SEQ[name][2] = str(chrEnd)
         else:
             pass
 

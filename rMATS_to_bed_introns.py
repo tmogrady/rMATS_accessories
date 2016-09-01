b87d436d294e96717f9711c19b02f65ab3b70e48
tmogrady <christina.ogrady@gmail.com>
2016-09-01 13:41:03 +0200

    now selects (only) longest upstream and longest downstream intron for each exon

--- rMATS_to_bed_introns.py
+++ rMATS_to_bed_introns.py
@@ -28,41 +28,117 @@ if size:
 else:
     output = open("%s_introns.bed" % name_stub, 'w') #open output file
 
+#dictionaries used for duplicate removal...
+UPSTREAM = {}
+DOWNSTREAM = {}
+#but! Can nest dictionaries. So try that later to make things more concise
+
 for line in open_file:
     line = line.rstrip("\n") #removes carriage return
     fields = line.split("\t")
     chr = fields[3][3:]
     if fields[0] == 'ID': #skips header lines
         pass
-    name = fields[1] + "|" + chr + ":" + fields[5] + "-" + fields[6] #later, add in "ex" and "in"
+    name = fields[1] + "|" + chr + fields[4] + fields[5] + "-" + fields[6] #later, add in "ex" and "in"
     if fields[4] == "+":
         if size:
             upper = int(fields[5]) - size #gets coordinates for x bp upstream...
             lower = int(fields[6]) + size #...and downstream
         if size and (upper >= int(fields[8])):  # or the whole intron, whichever is shorter. Is this int() necessary? Does fields[8] start as a string?
-            output.write(chr + "\t" + str(upper) + "\t" + fields[5] + "\t" + name + "|" + "up" + "\t" + fields[22] + "\t" + fields[4] + "\n")
+            intron = [chr, upper, fields[5], name, fields[22], fields[4]]
+            if name in UPSTREAM:#checks to see if this exon has been encountered before
+                if int(UPSTREAM[name][2]) - int(UPSTREAM[name][1]) < int(intron[2]) - int(intron[1]): #if so, checks to see if the current intron is longer than the previously see intron
+                    UPSTREAM[name] = intron #if so, replaces the previous intron with the current intron
+                else:
+                    pass #ignores current intron if it's shorter than one previously seen for that exon
+            else:
+                UPSTREAM[name] = intron #if this exon hasn't been seen before, adds it to the dictionary with its intron
+#output.write(chr + "\t" + str(upper) + "\t" + fields[5] + "\t" + name + "|" + "up" + "\t" + fields[22] + "\t" + fields[4] + "\n")
         else:
-            output.write(chr + "\t" + fields[8] + "\t" + fields[5] + "\t" + name + "|" + "up" + "\t" + fields[22] + "\t" + fields[4] + "\n")
+            intron = [chr, fields[8], fields[5], name, fields[22], fields[4]]
+            if name in UPSTREAM:#checks to see if this exon has been encountered before
+                if int(UPSTREAM[name][2]) - int(UPSTREAM[name][1]) < int(intron[2]) - int(intron[1]): #if so, checks to see if the current intron is longer than the previously see intron
+                    UPSTREAM[name] = intron #if so, replaces the previous intron with the current intron
+                else:
+                    pass #ignores current intron if it's shorter than one previously seen for that exon
+            else:
+                UPSTREAM[name] = intron #if this exon hasn't been seen before, adds it to the dictionary with its intron
+#output.write(chr + "\t" + fields[8] + "\t" + fields[5] + "\t" + name + "|" + "up" + "\t" + fields[22] + "\t" + fields[4] + "\n")
         if size and (lower <= int(fields[9])):
-            output.write(chr + "\t" + fields[6] + "\t" + str(lower) + "\t" + name + "|" + "down" + "\t" + fields[22] + "\t" + fields[4] + "\n")
+            intron = [chr, fields[6], lower, name, fields[22], fields[4]]
+            if name in DOWNSTREAM:
+                if int(DOWNSTREAM[name][2]) - int(DOWNSTREAM[name][1]) < int(intron[2]) - int(intron[1]):
+                    DOWNSTREAM[name] = intron
+                else:
+                    pass
+            else:
+                DOWNSTREAM[name] = intron
+#output.write(chr + "\t" + fields[6] + "\t" + str(lower) + "\t" + name + "|" + "down" + "\t" + fields[22] + "\t" + fields[4] + "\n")
         else:
-            output.write(chr + "\t" + fields[6] + "\t" + fields[9] + "\t" + name + "|" "down" + "\t" + fields[22] + "\t" + fields[4] + "\n")
+            intron = [chr, fields[6], fields[9], name, fields[22], fields[4]]
+            if name in DOWNSTREAM:
+                if int(DOWNSTREAM[name][2]) - int(DOWNSTREAM[name][1]) < int(intron[2]) - int(intron[1]):
+                    DOWNSTREAM[name] = intron
+                else:
+                    pass
+            else:
+                DOWNSTREAM[name] = intron
+            #output.write(chr + "\t" + fields[6] + "\t" + fields[9] + "\t" + name + "|" "down" + "\t" + fields[22] + "\t" + fields[4] + "\n")
     elif fields[4] == "-":
         if size:
             upper = int(fields[6]) + size
             lower = int(fields[5]) - size
-        name = fields[1] + "|" + chr + ":" + fields[5] + "-" + fields[6]
+        #name = fields[1] + "|" + chr + ":" + fields[5] + "-" + fields[6]
         if size and (upper <= int(fields[9])): #not sure if int is necessary
-            output.write(chr + "\t" + fields[6] + "\t" + str(upper) + "\t" + name + "|" + "up" + "\t" + fields[22] + "\t" + fields[4] + "\n")
+            intron = [chr, fields[6], upper, name, fields[22], fields[4]]
+            if name in UPSTREAM:
+                if int(UPSTREAM[name][2]) - int(UPSTREAM[name][1]) < int(intron[2]) - int(intron[1]): #if so, checks to see if the current intron is longer than the previously see intron
+                    UPSTREAM[name] = intron #if so, replaces the previous intron with the current intron
+                else:
+                    pass #ignores current intron if it's shorter than one previously seen for that exon
+            else:
+                UPSTREAM[name] = intron #if this exon hasn't been seen before, adds it to the dictionary with its intron
+        #output.write(chr + "\t" + fields[6] + "\t" + str(upper) + "\t" + name + "|" + "up" + "\t" + fields[22] + "\t" + fields[4] + "\n")
         else:
-            output.write(chr + "\t" + fields[6] + "\t" + fields[9] + "\t" + name + "|" + "up" + "\t" + fields[22] + "\t" + fields[4] + "\n")
+            intron = [chr, fields[6], fields[9], name, fields[22], fields[4]]
+            if name in UPSTREAM:
+                if int(UPSTREAM[name][2]) - int(UPSTREAM[name][1]) < int(intron[2]) - int(intron[1]): #if so, checks to see if the current intron is longer than the previously see intron
+                    UPSTREAM[name] = intron #if so, replaces the previous intron with the current intron
+                else:
+                    pass #ignores current intron if it's shorter than one previously seen for that exon
+            else:
+                UPSTREAM[name] = intron #if this exon hasn't been seen before, adds it to the dictionary with its intron
+                    #output.write(chr + "\t" + fields[6] + "\t" + fields[9] + "\t" + name + "|" + "up" + "\t" + fields[22] + "\t" + fields[4] + "\n")
         if size and (lower >= int(fields[7])):
-            output.write(chr + "\t" + str(lower) + "\t" + fields[5] + "\t" + name + "|" + "down" + "\t" + fields[22] + "\t" + fields[4] + "\n")
+            intron = [chr, lower, fields[5], name, fields[22], fields[4]]
+            if name in DOWNSTREAM:
+                if int(DOWNSTREAM[name][2]) - int(DOWNSTREAM[name][1]) < int(intron[2]) - int(intron[1]):
+                    DOWNSTREAM[name] = intron
+                else:
+                    pass
+            else:
+                DOWNSTREAM[name] = intron
+        #output.write(chr + "\t" + str(lower) + "\t" + fields[5] + "\t" + name + "|" + "down" + "\t" + fields[22] + "\t" + fields[4] + "\n")
         else:
-            output.write(chr + "\t" + fields[8] + "\t" + fields[5] + "\t" + name + "|" + "down" + "\t" + fields[22] + "\t" + fields[4] + "\n")
+            intron = [chr, fields[8], fields[5], name, fields[22], fields[4]]
+            if name in DOWNSTREAM:
+                if int(DOWNSTREAM[name][2]) - int(DOWNSTREAM[name][1]) < int(intron[2]) - int(intron[1]):
+                    DOWNSTREAM[name] = intron
+                else:
+                    pass
+            else:
+                DOWNSTREAM[name] = intron
+#output.write(chr + "\t" + fields[8] + "\t" + fields[5] + "\t" + name + "|" + "down" + "\t" + fields[22] + "\t" + fields[4] + "\n")
 
 #need to:
 #figure out how to deal with duplicates
+
+#output.write('\n'.join('%s %s' % x for x in sorted_Z_DICT))
+
+for key in UPSTREAM:
+    output.write("\t".join(UPSTREAM[key]) + "\n")
+for key in DOWNSTREAM:
+    output.write("\t".join(DOWNSTREAM[key]) + "\n")
 
 open_file.close()
 output.close()

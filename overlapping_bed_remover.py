6d218b2d699424e57b1a9cfec37be1ab366709a6
tmogrady <christina.ogrady@gmail.com>
2016-09-02 12:18:35 +0200

    adding comments

--- overlapping_bed_remover.py
+++ overlapping_bed_remover.py
@@ -1,4 +1,6 @@
 # Given a bed file, finds overlapping features and randomly selects one to return. Bed file must be sorted by chromosome then start and end position
+# to improve: make script sort the file first in case user hasn't
+# USAGE: overlapping_bed_remover.py <bed_file.bed>
 
 import sys, random
 
@@ -21,21 +23,19 @@ for line in open_file:
     fields = line.split("\t")
     if len(cluster) == 0: #adds values for first feature to the cluster
         cluster.append([fields[0], fields[5], fields[1], fields[2]])
-    else: #checks to see if current feature overlaps previous feature
+    else: #checks to see if current feature overlaps other features in the cluster
         chrStart = min([cluster[x][2] for x in range(0, len(cluster))])
         chrEnd = max([cluster[x][3] for x in range(0, len(cluster))])
         if fields[0]==cluster[0][0] and fields[5]== cluster[0][1] and ((chrStart <= fields[1] <= chrEnd) or (chrStart <= fields[2] <= chrEnd)):
             cluster.append([fields[0], fields[5], fields[1], fields[2]])
-        else:
+        else: #randomly choose one of the features in the cluster to print out
             random_index = random.randint(0, len(cluster)-1)
             bed_line = cluster[random_index][0] + "\t" + cluster[random_index][2] + "\t" + cluster[random_index][3] + "\t" + cluster[random_index][0]+":"+cluster[random_index][2]+"-"+cluster[random_index][3] + "\t1\t" + cluster[random_index][1] + "\n"
             output.write(bed_line)
-            #output.write("\t".join(cluster[random_index])+"\n")
             cluster = []
 
 random_index = random.randint(0, len(cluster)-1) #print out last feature
 bed_line = cluster[random_index][0] + "\t" + cluster[random_index][2] + "\t" + cluster[random_index][3] + "\t" + cluster[random_index][0]+":"+cluster[random_index][2]+"-"+cluster[random_index][3] + "\t1\t" + cluster[random_index][1] + "\n"
-#output.write("\t".join(cluster[random_index])+"\n")
 
 open_file.close()
 output.close()
